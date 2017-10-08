5bb70c23b07e734314f702f8e60be297c2153742
Luis Sanchez <sanchezl391@gmail.com>
2017-10-08 12:05:24 -0600

    Printing now works properly

--- tree.c
+++ tree.c
@@ -5,21 +5,14 @@
 #include "header.h"
 
 void printTree(Node* root){
-  int i = 0;
-  if(!root) printf("No values in tree");//Not sure
+  if(!root)
+    return;
   
   if(root->left)
-    return printTree(root->left);  
-  else if(!root->right){
-    printf("\n %s" , root->strValue);
-    i++;
-    }
-  
+    printTree(root->left);
+  printf("\n%s" , root->strValue);
   if(root->right)
-    return printTree(root->right);
-  else if(i)
-    printf("\n %s" , root->strValue);
-   
+    printTree(root->right);
  }
 
 void traverseTree(){
@@ -32,27 +25,20 @@ Node* createNode(char* str){
   createdNode->strValue = str;
   createdNode->right = 0;
   createdNode->left = 0;
-
-  printf("\nNode Created. Value: %s" , createdNode->strValue); 
+ 
   return createdNode;
 }
  
 
 Node* insertNode(Node* root, char* str){
-  if(root == 0){
-    printf("\nNo child nodes. Attempting to create Node");
-    root = createNode(str);    
-  }
-  else if(strcmp(str, root->strValue) > 0){
-    printf("\nCreating right child for %s" , root->strValue);
-    root->right = insertNode(root->right , str);
-  }
-  else{
-    printf("\nCreating left child for %s" ,root->strValue);
-    root->left = insertNode(root->left, str);
-  }
- 
-  printf("\n" ); 
+  if(!root)
+    root = createNode(str);
+
+  else if(strcmp(str, root->strValue) <= 0)
+    root->left = insertNode(root->left , str);
+  else
+    root->right = insertNode(root->right, str);
+
   return root;
 }
 
@@ -60,14 +46,10 @@ void main(){
   // char str[100];
   Node* root = 0;
 
-  root = insertNode(root , "ad");
-  root = insertNode(root,"sdfdsf");
-  root = insertNode(root, "sdfdsfdsfdsffdf");
-  root = insertNode(root , "a");
+  root = insertNode(root , "amy sand");
+  root = insertNode(root,"luis sanchez ");
+  root = insertNode(root, "susana gutierrez");
+  root = insertNode(root , "pewie");
   
-  printf("\nRoot: %s" , root->strValue);
-  printf("\nRoot's left child: %s" , root->left->strValue);
-  printf("\nRoot's right child: %s" , root->right->strValue);
-  printf("\nRoot's right child->right child: %s" , root->right->right->strValue);
-  
+  printTree(root);
 }
